2f0937aa8c145fd93c155f3f04d7e4ababe05b1d
KaKatheMorenoV <morenokathe07@gmail.com>
2024-06-10 14:50:46 -0500

    Implementar funcionalidad de usuarios

--- app/src/main/java/com/xidne/happyhealth/MainActivity.kt
+++ app/src/main/java/com/xidne/happyhealth/MainActivity.kt
@@ -3,24 +3,31 @@ package com.xidne.happyhealth
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+    var firebaseUser : FirebaseUser? = null
+    private lateinit var etEmailMain : EditText
+    private lateinit var etPasswordMain : EditText
+    private lateinit var btnLoginMain : Button
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        InicializarVariables()
 
-
-        val btnLogin: Button = findViewById(R.id.btnLoginMain)
-        btnLogin.setOnClickListener {
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
+        btnLoginMain.setOnClickListener { 
+            ValidarDatos()
         }
 
         val btnRegister: Button = findViewById(R.id.btnRegisterMain)
@@ -28,6 +35,60 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+
+
+    }
+
+    private fun ComprobarSesion(){
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        if (firebaseUser!=null){
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Sesión activa", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        ComprobarSesion()
+        super.onStart()
+    }
+
+    private fun InicializarVariables(){
+        etEmailMain = findViewById(R.id.etEmailMain)
+        etPasswordMain = findViewById(R.id.etPasswordMain)
+        btnLoginMain = findViewById(R.id.btnLoginMain)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun ValidarDatos() {
+        val email: String = etEmailMain.text.toString()
+        val password: String = etPasswordMain.text.toString()
+        
+        if (email.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese su correo electrónico", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese su contraseña", Toast.LENGTH_SHORT).show()
+        } else {
+            LoginUsuario(email,password)
+        }
+    }
+
+    private fun LoginUsuario(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {task->
+            if(task.isSuccessful){
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                Toast.makeText(applicationContext, "Ha iniciado sesión", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(applicationContext, "Ingrese los datos correctos", Toast.LENGTH_SHORT).show()
+            }
+
+        }.addOnFailureListener {e->
+            Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/xidne/happyhealth/RegisterActivity.kt
+++ app/src/main/java/com/xidne/happyhealth/RegisterActivity.kt
@@ -2,34 +2,112 @@ package com.xidne.happyhealth
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isEmpty
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var reference: DatabaseReference
+    private lateinit var  etName : EditText
+    private lateinit var  etEmail : EditText
+    private lateinit var  etPassword : EditText
+    private lateinit var  btnRegister : Button
+    private lateinit var  userType: Spinner
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        InicializarVariables()
+        val optionsUser = arrayOf("¿Eres doctor o paciente?","Doctor","Paciente")
+        userType.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, optionsUser)
 
-        // Referencia al botón de registro
-        val btnRegister: Button = findViewById(R.id.btnRegister)
         btnRegister.setOnClickListener {
-            Log.d("RegisterActivity", "Register button clicked")
-            // Iniciar HomeActivity y cerrar RegisterActivity
-            val intent = Intent(this@RegisterActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish() // Terminar RegisterActivity para que no se pueda volver con el botón de retroceso
-        }
-
-        // Referencia al botón de inicio de sesión
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        btnLogin.setOnClickListener {
-            Log.d("RegisterActivity", "Login button clicked")
-            // Iniciar MainActivity
-            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            validarDatos()
         }
     }
+
+
+    private fun InicializarVariables(){
+        etName = findViewById(R.id.etName)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnRegister = findViewById(R.id.btnRegister)
+        userType = findViewById(R.id.userType)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun validarDatos() {
+        val nombreUsuario : String = etName.text.toString()
+        val email : String = etEmail.text.toString()
+        val password : String = etPassword.text.toString()
+
+        if (nombreUsuario.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese nombre de usuario",Toast.LENGTH_SHORT).show()
+        } else if (email.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese un correo electrónico",Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese una contraseña",Toast.LENGTH_SHORT).show()
+        } else if (userType.selectedItem == "¿Eres doctor o paciente?"){
+            Toast.makeText(applicationContext, "Elija una opción de doctor o paciente",Toast.LENGTH_SHORT).show()
+        } else {
+            registrarUsuario(email, password)
+        }
+    }
+
+    private fun registrarUsuario(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                var uid : String = ""
+                uid = auth.currentUser!!.uid
+                reference = FirebaseDatabase.getInstance().reference.child("Users").child(uid)
+
+                val hashmap = HashMap<String, Any>()
+                val hEtName : String = etName.text.toString()
+                val hEtEmail : String = etEmail.text.toString()
+                val hEtPassword : String = etPassword.text.toString()
+                val hUserType : String = userType.selectedItem.toString()
+
+                hashmap["uid"] = uid
+                hashmap["n_usuario"] = hEtName
+                hashmap["email"] = hEtEmail
+                hashmap["password"] = hEtPassword
+                hashmap["userType"] = hUserType
+                hashmap["image"] = ""
+                hashmap["searchUser"] = hEtName.lowercase()
+
+                /*Nuevos datos de usuario*/
+                hashmap["nombres"]=""
+                hashmap["apellidos"] = ""
+                hashmap["edad"] = ""
+                hashmap["profesion"] = ""
+
+                reference.updateChildren(hashmap).addOnCompleteListener { task2->
+                    if (task2.isSuccessful){
+                        val intent = Intent(this@RegisterActivity, HomeActivity::class.java)
+                        Toast.makeText(applicationContext, "Se ha registrado con éxito",Toast.LENGTH_SHORT).show()
+                        startActivity(intent)
+                    }
+                }.addOnFailureListener {e->
+                    Toast.makeText(applicationContext, "${e.message}",Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(applicationContext, "Ha ocurrido un error",Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {e->
+            Toast.makeText(applicationContext, "${e.message}",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
 
